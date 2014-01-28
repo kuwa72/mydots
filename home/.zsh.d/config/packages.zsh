7f066f4993606e7e945d609b4e9c03695fae75de
KUWASHIMA Yuichiro <ykuwashima@gmail.com>
2014-01-28 18:42:28 +0900

    * Zsh: Remove up/down bindkey of history-substring-search.

--- home/.zsh.d/config/packages.zsh
+++ home/.zsh.d/config/packages.zsh
@@ -21,11 +21,6 @@ zle-line-init() {
 }
 zle -N zle-line-init
 
-# bind UP and DOWN arrow keys
-zmodload zsh/terminfo
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-
 # bind P and N for EMACS mode
 bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
